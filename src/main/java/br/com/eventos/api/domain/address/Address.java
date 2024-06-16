4a43e1b4933b511603b48812014c69182a14d6e1
Vitor Staub <vitorhugostaub@hotmail.com>
2024-06-16 10:35:55 -0300

    feat: creating entities

---
+++ src/main/java/br/com/eventos/api/domain/address/Address.java
@@ -0,0 +1,30 @@
+package br.com.eventos.api.domain.address;
+
+import br.com.eventos.api.domain.event.Event;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/br/com/eventos/api/domain/coupon/Coupon.java
@@ -0,0 +1,32 @@
+package br.com.eventos.api.domain.coupon;
+
+import br.com.eventos.api.domain.event.Event;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupon")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
