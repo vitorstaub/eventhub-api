1189f969cde43c3d2c0b4c4e06ef34f09b9c4d8a
Vitor Staub <vitorhugostaub@hotmail.com>
2024-06-17 14:59:45 -0300

    feat: add method to find valid coupons

--- src/main/java/br/com/eventhub/api/repositories/CouponRepository.java
+++ src/main/java/br/com/eventhub/api/repositories/CouponRepository.java
@@ -3,7 +3,11 @@ package br.com.eventhub.api.repositories;
 import br.com.eventhub.api.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+
+    List<Coupon> findByEventIdAndValidAfter(UUID eventId, Date currentDate);
 }
