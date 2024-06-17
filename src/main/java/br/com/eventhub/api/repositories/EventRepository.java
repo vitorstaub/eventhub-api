d663359658f929c7af2c38272baa8ca58996508f
Vitor Staub <vitorhugostaub@hotmail.com>
2024-06-17 09:05:15 -0300

    feat: add filter to current date

--- src/main/java/br/com/eventhub/api/repositories/EventRepository.java
+++ src/main/java/br/com/eventhub/api/repositories/EventRepository.java
@@ -1,9 +1,17 @@
 package br.com.eventhub.api.repositories;
 
 import br.com.eventhub.api.domain.event.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
+
+    @Query("SELECT e FROM Event e WHERE e.date >= :currentDate")
+    public Page<Event> findUpcomingEvents(@Param("currentDate")Date currentDate, Pageable pageable);
 }
