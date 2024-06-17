0b87b0feafd6b79721660676c7291070a85f9109
Vitor Staub <vitorhugostaub@hotmail.com>
2024-06-17 14:58:46 -0300

    feat: add event details DTO and left join on table to get address

--- src/main/java/br/com/eventhub/api/domain/event/Event.java
+++ src/main/java/br/com/eventhub/api/domain/event/Event.java
@@ -1,9 +1,7 @@
 package br.com.eventhub.api.domain.event;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import br.com.eventhub.api.domain.address.Address;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,4 +28,6 @@ public class Event {
     private Boolean remote;
     private Date date;
 
+    @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+    private Address address;
 }
--- src/main/java/br/com/eventhub/api/repositories/EventRepository.java
+++ src/main/java/br/com/eventhub/api/repositories/EventRepository.java
@@ -12,6 +12,6 @@ import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
 
-    @Query("SELECT e FROM Event e WHERE e.date >= :currentDate")
+    @Query("SELECT e FROM Event e LEFT JOIN FETCH e.address a WHERE e.date >= :currentDate")
     public Page<Event> findUpcomingEvents(@Param("currentDate")Date currentDate, Pageable pageable);
 }
