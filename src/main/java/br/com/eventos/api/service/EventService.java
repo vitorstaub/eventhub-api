2214f9b997323f4cc6df82f41073383a66e97351
Vitor Staub <vitorhugostaub@hotmail.com>
2024-06-16 21:13:51 -0300

    fix: add new event remote and save repository

--- src/main/java/br/com/eventos/api/service/EventService.java
+++ src/main/java/br/com/eventos/api/service/EventService.java
@@ -3,6 +3,7 @@ package br.com.eventos.api.service;
 import br.com.eventos.api.domain.event.Event;
 
 import br.com.eventos.api.domain.event.EventRequestDTO;
+import br.com.eventos.api.repositories.EventRepository;
 import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +14,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -23,6 +25,10 @@ public class EventService {
 
     @Autowired
     private AmazonS3 s3Client;
+
+    @Autowired
+    private EventRepository repository;
+
     public Event createEvent(EventRequestDTO data) {
         String imgUrl = null;
 
@@ -34,7 +40,11 @@ public class EventService {
         newEvent.setTitle(data.title());
         newEvent.setDescription(data.description());
         newEvent.setEventUrl(data.eventUrl());
+        newEvent.setDate(new Date(data.date()));
         newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(data.remote());
+
+        repository.save(newEvent);
 
         return newEvent;
     }
