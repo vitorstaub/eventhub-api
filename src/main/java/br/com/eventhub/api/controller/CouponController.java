d0adb7f304551e9e286bbf6f008baa0c329f715c
Vitor Staub <vitorhugostaub@hotmail.com>
2024-06-17 08:04:35 -0300

    feat: add coupon controller

---
+++ src/main/java/br/com/eventhub/api/controller/CouponController.java
@@ -0,0 +1,25 @@
+package br.com.eventhub.api.controller;
+
+import br.com.eventhub.api.domain.coupon.Coupon;
+import br.com.eventhub.api.domain.coupon.CouponRequestDTO;
+import br.com.eventhub.api.service.CouponService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/coupon")
+public class CouponController {
+
+    @Autowired
+    private CouponService couponService;
+
+    @PostMapping("/event/{eventId}")
+    public ResponseEntity<Coupon> addCouponsToEvent(@PathVariable UUID eventId, @RequestBody CouponRequestDTO data) {
+        Coupon coupons = couponService.addCouponToEvent(eventId, data);
+        return ResponseEntity.ok(coupons);
+    }
+}
